20a49e981af759be594813ac83b3f8b342935f94
Matthew Wagaman <averaqedev@gmail.com>
2020-02-06 09:00:41 -0800

    Added 'Add Folder' Component

--- src/App/App.js
+++ src/App/App.js
@@ -8,6 +8,7 @@ import NotePageMain from '../NotePageMain/NotePageMain';
 import ApiContext from '../ApiContext';
 import config from '../config';
 import './App.css';
+import AddFolder from '../AddFolder/AddFolder';
 
 class App extends Component {
     state = {
@@ -42,6 +43,12 @@ class App extends Component {
         });
     };
 
+    addFolder = folder => {
+        this.setState({
+            folders: [...this.state.folders, folder]
+        })
+    }
+
     renderNavRoutes() {
         return (
             <>
@@ -72,6 +79,7 @@ class App extends Component {
                     />
                 ))}
                 <Route path="/note/:noteId" component={NotePageMain} />
+                <Route path="/add-folder" component={AddFolder} />
             </>
         );
     }
@@ -80,7 +88,8 @@ class App extends Component {
         const value = {
             notes: this.state.notes,
             folders: this.state.folders,
-            deleteNote: this.handleDeleteNote
+            deleteNote: this.handleDeleteNote,
+            addFolder: this.addFolder
         };
         return (
             <ApiContext.Provider value={value}>
